5665ad03919361714738ee9239aac3bf7f93eb60
ken.kamiya <ken.kamiya@kraken.tech>
2025-10-05 20:17:21 +0900

    Bugfix to get page title

--- src/features/tasks/utils/getPageTitle.ts
+++ src/features/tasks/utils/getPageTitle.ts
@@ -1,5 +1,6 @@
+const base_url = import.meta.env.VITE_API_BASE_URL;
 export async function getPageTitle(url: string) {
-    const response = await fetch(`http://localhost:8890/api/v2/getPageTitle/?url=${url}`);
+    const response = await fetch(`${base_url}/getPageTitle/?url=${url}`);
     const data = await response.json();
     return data.title;
 }
