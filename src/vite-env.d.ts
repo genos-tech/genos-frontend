903292932f2ef5a48103dfeddfbc0a11cfea31ec
ken.kamiya <ken.kamiya@kraken.tech>
2026-04-25 10:16:59 +0900

    Major bugfix v1

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
     readonly VITE_API_BASE_URL: string;
     readonly VITE_WS_BASE_URL: string;
     readonly VITE_API_DJANGO_BASE_URL: string;
+    readonly VITE_DJANGO_URL: string;
+    readonly VITE_MEDIA_ROOT_DJANGO: string;
 }
 
 interface ImportMeta {
