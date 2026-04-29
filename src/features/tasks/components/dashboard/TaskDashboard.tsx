6176ca066a43ba0c5eed3d1202c06e744c78f1d4
ken.kamiya <ken.kamiya@kraken.tech>
2026-04-29 12:49:39 +0900

    Use user avator for task dashboard

--- src/features/tasks/components/dashboard/TaskDashboard.tsx
+++ src/features/tasks/components/dashboard/TaskDashboard.tsx
@@ -1,14 +1,44 @@
+import { Socket } from "socket.io-client";
+
+import { ChatManagementState } from "../../../../hooks/chats/useChatManagement";
 import { ProjectManagementState } from "../../../../hooks/common/useProjectManagement";
 import { TeamManagementState } from "../../../../hooks/common/useTeamManagement";
+import { UIStateManagementState } from "../../../../hooks/common/useUIStateManagement";
 import { TaskManagementState } from "../../../../hooks/tasks/useTaskManagement";
+import { UserProps } from "../../../../types/admin";
 import { TaskHomeContent } from "./TaskHomeContent";
 
 type TaskDashboardProps = {
     useTM: TaskManagementState;
     usePM: ProjectManagementState;
     useTEM: TeamManagementState;
+    myself: UserProps;
+    setMyself: (me: UserProps) => void;
+    useCM: ChatManagementState;
+    useUISM: UIStateManagementState;
+    socket: Socket | null;
 };
 
-export const TaskDashboard = ({ useTM, usePM, useTEM }: TaskDashboardProps) => {
-    return <TaskHomeContent useTM={useTM} usePM={usePM} useTEM={useTEM} />;
+export const TaskDashboard = ({
+    useTM,
+    usePM,
+    useTEM,
+    myself,
+    setMyself,
+    useCM,
+    useUISM,
+    socket,
+}: TaskDashboardProps) => {
+    return (
+        <TaskHomeContent
+            useTM={useTM}
+            usePM={usePM}
+            useTEM={useTEM}
+            myself={myself}
+            setMyself={setMyself}
+            useCM={useCM}
+            useUISM={useUISM}
+            socket={socket}
+        />
+    );
 };
